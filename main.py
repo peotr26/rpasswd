5d19d943140c1399bec5a4669470acc2389ce30b
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-03-18 13:20:39 +0100

    Added arguments support

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 '''Program to generate a random password.'''
 
 
+import sys
 import secrets
 
-
-alphabet = [
+c_level = {'alphabet': [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-]
-
-alphanumerical = [
+],
+    'alphanumerical': [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-]
-
-complete = [
+],
+    'complete': [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     '!', '#', '$', '%', '&', '*', '+', '?', '@', '^',
-]
+]}
 
 
-def passwd(n: int, c: list) -> str:
+def passwd(n: int, c: dict) -> str:
     '''
     Main function which generate a random password using hardware.
 
@@ -40,5 +38,10 @@ def passwd(n: int, c: list) -> str:
     '''
     password = ''
     for i in range(n):
-        password += secrets.choice(c)
+        password += secrets.choice(c[sys.argv[2]])
     return password
+
+
+assert len(sys.argv) == 3
+
+print(passwd(int(sys.argv[1]), c_level))
