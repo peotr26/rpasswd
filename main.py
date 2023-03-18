8e130cdaaf31d664d3eb0529841478905ad43266
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-03-18 12:51:33 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+'''Program to generate a random password.'''
+
+
+import secrets
+
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+]
+
+alphanumerical = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+]
+
+complete = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '!', '#', '$', '%', '&', '*', '+', '?', '@', '^',
+]
+
+
+def passwd(n: int, c: list) -> str:
+    '''
+    Main function which generate a random password using hardware.
+
+    Args:
+        n (int) : The length of the password that is generated.
+        c (list) : The level of complexity of the password.
+            3 options of complexity : alphabet, alphanumerical, or complete.
+    '''
+    password = ''
+    for i in range(n):
+        password += secrets.choice(c)
+    return password
